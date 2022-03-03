19b5eda3a3d7fb17789320f52e13ea3843421c36
Elliott Chong <elliottchong16@gmail.com>
2022-03-03 17:49:38 +0800

    fix

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const rows = 6;
+const keys = document.querySelectorAll(".key");
 let word = "house";
 const columns = word.length;
 const board = document.getElementById("board");
@@ -23,22 +26,23 @@ let row = 0;
 let gameOver = false;
 let currentTile;
 
-document.addEventListener("keyup", (e) => {
+enterTile = (e) => {
   console.log(row, col);
   if (gameOver) return;
-  if (e.code === "Backspace") {
+  if (e.code === "Backspace" || e.code === "Back") {
     if (col > 0 && col <= columns && row <= 5) {
       currentTile = document.getElementById(
         row.toString() + "-" + (col - 1).toString()
       );
       if (currentTile.innerText != "") {
         currentTile.innerText = "";
+        currentTile.classList.remove("filled");
         col--;
       }
       return;
     }
   }
-  if (col == columns && e.key == "Enter") {
+  if (col == columns && e.code == "Enter") {
     check(row);
     col = 0;
     row++;
@@ -49,26 +53,62 @@ document.addEventListener("keyup", (e) => {
       currentTile = document.getElementById(
         row.toString() + "-" + col.toString()
       );
-      if (currentTile.innerText === "")
-        currentTile.innerText = e.key.toUpperCase();
+      if (currentTile.innerText === "") currentTile.classList.add("filled");
+      currentTile.innerText = e.key.toUpperCase();
 
       col++;
     }
   }
-});
+};
 
-const check = (row) => {
+document.addEventListener("keyup", enterTile);
+
+keys.forEach((key) =>
+  key.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    enterTile({ key: e.target.innerText.toLowerCase() });
+  })
+);
+
+const specialKeys = document.querySelectorAll(".key2");
+specialKeys.forEach((key) =>
+  key.addEventListener("click", (e) => {
+    enterTile({ code: e.target.innerText });
+  })
+);
+
+const check = async (row) => {
+  let animation_duration = 150;
   let correct = 0;
   for (let col = 0; col < columns; col++) {
     currentTile = document.getElementById(
       row.toString() + "-" + col.toString()
     );
-    if (currentTile.innerText.toLowerCase() == word[col]) {
+    let correspondingKey = document.querySelector(
+      `[data-letter=${currentTile.innerText.toUpperCase()}]`
+    );
+    let classL = correspondingKey.classList;
+    let cont =
+      !classL.contains("inPlace") ||
+      !classL.contains("present") ||
+      !classL.contains("wrong");
+    currentTile.classList.add("flip");
+    setTimeout(() => currentTile.classList.remove("flip"), animation_duration);
+    await sleep(animation_duration);
+    if (currentTile.innerText.toLowerCase() == word[col].toLowerCase()) {
+      if (cont) correspondingKey.classList.add("inPlace");
       correct++;
       currentTile.classList.add("inPlace");
-    } else if (word.includes(currentTile.innerText.toLowerCase())) {
+    } else if (
+      word.toLowerCase().includes(currentTile.innerText.toLowerCase())
+    ) {
+      if (cont) correspondingKey.classList.add("present");
       currentTile.classList.add("present");
-    } else currentTile.classList.add("wrong");
+    } else {
+      currentTile.classList.add("wrong");
+      if (cont) correspondingKey.classList.add("wrong");
+    }
+    await sleep(100);
   }
   if (correct == word.length) {
     gameOver = true;
@@ -77,8 +117,7 @@ const check = (row) => {
     dgameOver("lose");
   }
 };
-const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const dgameOver = async (condition) => {
-  document.getElementById("answer").innerText = word;
+  document.getElementById("answer").innerText = word.toUpperCase();
 };
